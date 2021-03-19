aca46eb5d8a66fd7070dffb5e010d52330ff97f6
causehhc <1005694755@qq.com>
2021-03-20 01:50:57 +0800

    完成上线测试 static部分仍有bug

--- app.py
+++ app.py
@@ -3,9 +3,22 @@ import requests
 from flask_cors import CORS
 from random import *
 
-app = Flask(__name__, template_folder='../frontend/dist', static_folder='../frontend/dist')
+app = Flask(__name__, template_folder='../frontend/dist', static_folder='../dist/frontend/static')
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+some_list = []
+lens = 20
+for i in range(lens):
+    item = {
+        'body': {
+            'content': '',
+            'totalPages': ''
+        },
+    }
+    item['body']['content'] = 'page{}'.format(i)
+    item['body']['totalPages'] = '{}'.format(lens)
+    some_list.append(item)
 
 
 @app.route('/api/random')
@@ -16,12 +29,26 @@ def random_number():
     return jsonify(response)
 
 
+idx = 0
+
+
+@app.route('/api/info')
+def msg_load():
+    global idx
+    response = some_list[idx]
+    idx += 1
+    if idx == len(some_list):
+        idx = 0
+    return jsonify(response)
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     if app.debug:
         return requests.get('http://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
+    # return 'hello'
 
 
 if __name__ == '__main__':
